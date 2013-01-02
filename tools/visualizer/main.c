30f030381f7bb8569887d486e7a539cb779c66ec
Thomas Buck <taucher.bodensee@googlemail.com>
2013-01-03 00:59:34 +0100

    Extended visualizer.

--- tools/visualizer/main.c
+++ tools/visualizer/main.c
@@ -34,46 +34,66 @@
 
 #include "serial.h"
 
-#define VERSION    "0.1"
-#define HEADING    "Visualizer - "VERSION // Is this really portable?
-#define SUBHEADING "Press q to exit!"
+#define VERSION      "0.1"
+#define HEADING      "Visualizer Version "VERSION // Is this really portable?
+#define SUBHEADING   "Press q to exit!"
+#define CONNECTED    "Connected"
+#define DISCONNECTED "Disconnected"
+#define HEADINGSIZE 4
 
-#define MAINREDRAW 0
-#define IDLE 1
+// Main Menu
+#define IDLE 0
+#define MAINREDRAW 1
 #define QUIT 2
 #define SELECT 3
 #define CONNECT 4
 
-int width, height;
+// Connect Menu
+//#define IDLE 0
+//#define MAINREDRAW 1
+//#define QUIT 2
 
-void printHeading() {
+int width, height, connected = 0;
+char *port = NULL;
+
+void printHeading(char *third) {
     attron(A_BOLD | A_UNDERLINE | COLOR_PAIR(1));
-    mvprintw(0, (width - strlen(HEADING)) / 2, "%s", HEADING);
+    //mvprintw(0, (width - strlen(HEADING)) / 2, "%s", HEADING);
+    mvprintw(0, 0, "%s", HEADING);
     attroff(A_BOLD | A_UNDERLINE | COLOR_PAIR(1));
-    mvprintw(1, (width - strlen(SUBHEADING)) / 2, "%s", SUBHEADING);
+    //mvprintw(1, (width - strlen(SUBHEADING)) / 2, "%s", SUBHEADING);
+    mvprintw(1, (strlen(HEADING) - strlen(SUBHEADING)) / 2, "%s", SUBHEADING);
+    //mvprintw(2, (width - strlen(third)) / 2, "%s", third);
+    mvprintw(2, (strlen(HEADING) - strlen(third)) / 2, "%s", third);
+
+    if (connected) {
+        mvprintw(3, (strlen(HEADING) - strlen(CONNECTED)) / 2, CONNECTED);
+    } else {
+        mvprintw(3, (strlen(HEADING) - strlen(DISCONNECTED)) / 2, DISCONNECTED);
+    }
 }
 
-int selectMenu(char **portVar) {
+int selectMenu(void) {
     char **ports = getSerialPorts();
     int i = 0;
     clear();
-    printHeading();
+    printHeading("Port Selection");
     while (ports[i] != NULL) {
-        mvprintw(i + 3, 0, "%i) %s", i, ports[i]);
+        mvprintw(i + HEADINGSIZE + 1, 0, "%i) %s", i, ports[i]);
         i++;
     }
     int choice = -1;
     do {
-        mvprintw(i + 4, 0, "Your choice: ");
+        mvprintw(i + HEADINGSIZE + 2, 0, "Your choice: ");
         clrtoeol();
         echo();
         scanw("%d", &choice);
         noecho();
         if (!((choice >= 0) && (choice < i))) {
-            mvprintw(i + 5, 0, "Invalid!");
+            mvprintw(i + HEADINGSIZE + 3, 0, "Invalid!");
         }
     } while (!((choice >= 0) && (choice < i)));
-    *portVar = ports[choice];
+    port = ports[choice];
     for (int a = 0; a < i; a++) {
         if (a != choice)
             free(ports[a]);
@@ -82,9 +102,42 @@ int selectMenu(char **portVar) {
     return MAINREDRAW;
 }
 
+int connectMenu(void) {
+    int ch, menu = MAINREDRAW;
+
+    do {
+        if (menu == MAINREDRAW) {
+            clear();
+            printHeading("Connection Menu");
+            mvprintw(HEADINGSIZE + 1, 0, "c) Connect");
+            mvprintw(HEADINGSIZE + 2, 0, "d) Disconnect");
+            mvprintw(HEADINGSIZE + 3, 0, "q) Back");
+            menu = IDLE;
+        }
+        refresh();
+        ch = getch();
+        switch (ch) {
+            case 'c': case 'C':
+                connected = 1;
+                menu = MAINREDRAW;
+                break;
+
+            case 'd': case 'D':
+                connected = 0;
+                menu = MAINREDRAW;
+                break;
+
+            case 'q': case 'Q':
+                menu = QUIT;
+                break;
+        }
+    } while (menu != QUIT);
+
+    return MAINREDRAW;
+}
+
 int main(int argc, char *argv[]) {
     int ch, menu = MAINREDRAW;
-    char *port = NULL;
 
     initscr();
     cbreak();
@@ -92,31 +145,40 @@ int main(int argc, char *argv[]) {
     nonl();
     intrflush(stdscr, FALSE);
     keypad(stdscr, TRUE);
+    curs_set(0);
     getmaxyx(stdscr, height, width);
     start_color();
     init_pair(1, COLOR_RED, COLOR_BLACK);
 
+    char **ports = getSerialPorts();
+    port = ports[0];
+    ch = 1;
+    while (ports[ch] != NULL) {
+        free(ports[ch++]);
+    }
+    free(ports);
+
     do {
         if (menu == MAINREDRAW) {
             clear();
-            printHeading();
-            mvprintw(3, 0, "0)  S  -  Select Serial Port (current: %s)", port);
-            mvprintw(4, 0, "1)  C  -  Connect");
-            mvprintw(5, 0, "2)  Q  -  Quit Visualizer");
+            printHeading("Main Menu");
+            mvprintw(HEADINGSIZE + 1, 0, "s) Select Serial Port (current: %s)", port);
+            mvprintw(HEADINGSIZE + 2, 0, "c) Connect");
+            mvprintw(HEADINGSIZE + 3, 0, "q) Quit Visualizer");
             menu = IDLE;
         }
         refresh();
         ch = getch();
         switch (ch) {
-            case 's': case 'S': case '0':
-                menu = selectMenu(&port);
+            case 's': case 'S':
+                menu = selectMenu();
                 break;
 
-            case 'c': case 'C': case '1':
-
+            case 'c': case 'C':
+                menu = connectMenu();
                 break;
 
-            case 'q': case 'Q': case '2':
+            case 'q': case 'Q':
                 menu = QUIT;
                 break;
         }
